f38a669dcb528460233e4ba3c45cf7e22002262e
nguyennhucuong25 <nhucuongtk47@gmail.com>
2025-03-28 17:22:54 +0700

    Add files via upload

---
+++ bài3/bài3_10.py
@@ -0,0 +1,19 @@
+import math
+
+def Tinh(R):
+    # Kiểm tra giá trị bán kính
+    if R <= 0:
+        return "Giá trị bán kính không hợp lệ. Bán kính phải lớn hơn 0."
+    
+    # Tính chu vi và diện tích
+    C = 2 * math.pi * R
+    S = math.pi * R**2
+    
+    return f"Chu vi: {C:.2f}, Diện tích: {S:.2f}"
+
+# Nhập bán kính từ bàn phím
+try:
+    R = float(input("Nhập bán kính R: "))
+    print(Tinh(R))
+except ValueError:
+    print("Giá trị nhập vào không hợp lệ. Vui lòng nhập số.")
---
+++ bài3/bài3_8.py
@@ -0,0 +1,21 @@
+import math 
+pos = [0,0] 
+while True: 
+ s = input("Vui lòng nhập lệnh di chuyển: ") 
+ if not s: 
+  break 
+ movement = s.split(" ") 
+ direction = movement[0] 
+ steps = int(movement[1]) 
+ if direction=="UP": 
+  pos[0]+=steps 
+ elif direction=="DOWN": 
+  pos[0]-=steps 
+ elif direction=="LEFT": 
+  pos[1]-=steps
+ elif direction=="RIGHT": 
+  pos[1]+=steps 
+ else: 
+  pass 
+###################### 
+print ("Khoang cách từ hiện tại vị trí đầu tiên là: ",int(round(math.sqrt(pos[1]**2+pos[0]**2))))
