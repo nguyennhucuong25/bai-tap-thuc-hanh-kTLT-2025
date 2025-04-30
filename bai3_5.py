2ae9c9cf64a63f0ad15493bf23c09d0cdaab492e
nguyennhucuong25 <nhucuongtk47@gmail.com>
2025-04-30 14:57:01 +0700

    Add files via upload

---
+++ bai3_5.py
@@ -0,0 +1,9 @@
+a="Hello Guy!"
+def say():
+    global a
+    a="Vinh University" 
+    print(a)
+say()
+print(a)
+print("nguyễn như cường")   
+print("MSSV: 235752020710047") 
---
+++ bài3_6.py
@@ -0,0 +1,10 @@
+def get_sum(*num):
+    tmp = 0
+    # duyet cac tham so
+    for i in num:
+        tmp +=i
+    return tmp
+result= get_sum(1,2,3,4,5,9)    
+print(result)
+print("nguyễn như cường")   
+print("MSSV: 235752020710047") 
