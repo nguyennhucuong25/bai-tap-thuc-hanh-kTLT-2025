036db74441bfe778a319ca595136092834d63824
nguyennhucuong25 <nhucuongtk47@gmail.com>
2025-04-30 14:56:00 +0700

    Delete bài3 directory

--- bài3/bài3_2.py
+++
@@ -1,7 +0,0 @@
-def sum(a, b):
-    return a + b
-
-c = sum(4, 5)
-
-# In kết quả
-print("tổng của 4 và 5 = " + str(c))
--- bài3/bài3_7.py
+++
@@ -1,7 +0,0 @@
-def checkValue(n):
-    if n % 2 == 0:
-        print("Đây là một số chẵn")
-    else:
-        print("Đây là một số lẻ")
-
-checkValue(10)
