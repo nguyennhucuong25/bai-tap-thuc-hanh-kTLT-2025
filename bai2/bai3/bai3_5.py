f188e84eabd0204f2fa43448f78d4d06050c745f
nguyennhucuong25 <nhucuongtk47@gmail.com>
2025-04-30 15:28:08 +0700

    Delete bai2/bai3 directory

--- bai2/bai3/bai3_5.py
+++
@@ -1,9 +0,0 @@
-a="Hello Guy!"
-def say():
-    global a
-    a="Vinh University" 
-    print(a)
-say()
-print(a)
-print("nguyễn như cường")   
-print("MSSV: 235752020710047") 
--- bai2/bai3/bài3_4.py
+++
@@ -1,8 +0,0 @@
-a="Hello Guy!"
-def say(a):
-    a="Vinh University"
-    print(a)
-say(a)
-print(a)
-print("nguyễn như cường")   
-print("MSSV: 235752020710047") 
