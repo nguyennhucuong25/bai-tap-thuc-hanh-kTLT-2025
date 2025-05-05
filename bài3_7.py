263cea845e12f8fe8f45268b7c1f389b8520ef94
nguyennhucuong25 <nhucuongtk47@gmail.com>
2025-05-05 20:52:44 +0700

    Delete bài3_7.py

--- bài3_7.py
+++
@@ -1,9 +0,0 @@
-def checkValue(n):
-    if n % 2 == 0:
-        print("Đây là một số chẵn")
-    else:
-        print("Đây là một số lẻ")
-
-checkValue(10)
-print("nguyễn như cường")   
-print("MSSV: 235752020710047") 
