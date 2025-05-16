b1f1da1263d37614368e583e01a6a30946b73050
nguyennhucuong25 <nhucuongtk47@gmail.com>
2025-05-16 09:40:14 +0700

    Update bài4_6.py

--- bai4/bài4_6.py
+++ bai4/bài4_6.py
@@ -1,12 +1,14 @@
 full_name = input('Nhập tên người: ')
 parts = full_name.split()
+
 if len(parts) >= 2:
-    last_name = parts[0]  
-    first_name = parts[1]  
+    last_name = parts[0]           # Họ (từ đầu tiên)
+    first_name = parts[-1]         # Tên riêng (từ cuối cùng)
 else:
-    last_name = full_name  
+    last_name = full_name
     first_name = ''
+
 print('Họ:', last_name)
 print('Tên riêng:', first_name)
 print("nguyễn như cường")   
-print("MSSV: 235752020710047")     
+print("MSSV: 235752020710047")
